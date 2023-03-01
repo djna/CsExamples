1b813fb81b2d9eb7dcb66a80e56ba9824429afaa
djnaSoft <88729193+djnaSoft@users.noreply.github.com>
2023-03-01 08:44:01 +0000

    skeleton

---
+++ CountdownSong/CountdownSong/Song.cs
@@ -0,0 +1,13 @@
+﻿namespace CountdownSong;
+public class Song
+{
+
+    public string Verse( int number){
+        return 
+            "2 cans of Lilt on the wall, " +
+        "2 cans of Lilt.\n" +
+        "Take one down and pass it around, " +
+        "1 cans of Lilt on the wall.\n";
+    }
+
+}
---
+++ CountdownSong/Singer/Singer.cs
@@ -0,0 +1,19 @@
+﻿using CountdownSong;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+      
+ 
+            Console.WriteLine("Singing ...");
+
+            Song mySong = new Song();
+        
+            string song = mySong.Verse(99);
+
+            Console.WriteLine($"{song}");
+
+        
+    }
+}
