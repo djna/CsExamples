ffa986933beb8f514a7cfafe2ce49a9df13d7dbc
djnaSoft <88729193+djnaSoft@users.noreply.github.com>
2023-03-02 09:24:23 +0000

    solution with potential for improvement

--- tennis/Tennis.Tests/TennisTests.cs
+++ tennis/Tennis.Tests/TennisTests.cs
@@ -55,9 +55,9 @@ namespace Tennis.Tests
 
         [Theory]
         [ClassData(typeof(TestDataGenerator))]
-        public void Tennis2Test(int p1, int p2, string expected)
+        public void Tennis3Test(int p1, int p2, string expected)
         {
-            var game = new TennisGame2("player1", "player2");
+            var game = new TennisGame3("player1", "player2");
             CheckAllScores(game, p1, p2, expected);
         }
 
