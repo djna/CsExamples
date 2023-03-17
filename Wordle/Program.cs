71283da5253ef7d4ecf3f88ec197b309fdc1c775
djnaSoft <88729193+djnaSoft@users.noreply.github.com>
2023-03-17 17:19:39 +0000

    wordlist from file

--- Wordle/Program.cs
+++ Wordle/Program.cs
@@ -1,10 +1,10 @@
-﻿namespace Wordle
+﻿using System.Text.RegularExpressions;
+namespace Wordle
 {
     class Program
     {
         static void Main(string[] args)
-        {
-            
+        {       
             Wordle theWordle = new Wordle();
 
             string action = "";
@@ -18,7 +18,6 @@
             }
 
              Console.WriteLine("Goodbye and thanks from Wordle!");
-
         }
     }
     class Wordle {
@@ -26,24 +25,37 @@
         public Wordle(){
             myWordList = new WordList();
             Console.WriteLine("Hello from Wordle!");
-
         }
 
         public void playWordle(){
             string target = myWordList.getRandomWord();
             Console.WriteLine("Apologies, playing not yet implemented. Target was {0}", target);
         }
-
-
     }
 
     class WordList {
         private string[] myWords;
         private Random random;
         public WordList(){
-            string[] someWords = new string[]{ "anvil", "boxes", "croft", "dance"};
-            myWords = someWords;
-            Console.WriteLine("WordList {0}", string.Join(",", myWords) );
+           
+            try {
+                string contents = File.ReadAllText("Wonder.txt");
+                string pattern = @"\b\w+\b";
+                myWords = Regex.Matches(contents, pattern)
+                       .Cast<Match>()
+                       .Select(m => m.Value)
+                       .Distinct()
+                       .ToArray();
+                Array.Sort(myWords);
+                
+            } catch (Exception e) {
+                Console.WriteLine("Error reading word list {0}", e.Message);
+                myWords = new string[]{ "anvil", "boxes", "croft", "dance"};
+            }
+            
+            Console.WriteLine("WordList {0} {1}", 
+                        myWords.Length,
+                        string.Join(",", myWords) );
             random = new Random();
         }
 
@@ -51,7 +63,5 @@
             int index = random.Next(myWords.Length);
             return myWords[index];
         }
-
-
     }
 }
