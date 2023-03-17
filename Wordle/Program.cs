853ff241a534fb2a851509e6f4d8e4a5184e8147
djnaSoft <88729193+djnaSoft@users.noreply.github.com>
2023-03-17 09:15:33 +0000

    skeleton Wordle

---
+++ Wordle/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Wordle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Wordle theWordle = new Wordle();
+
+            string action = "";
+            while( action != "X"){
+               
+                Console.WriteLine("(P)lay or e(X)it)");
+                action = Console.ReadLine().ToUpper();
+                if ( action == "P"){
+                    theWordle.playWordle();
+                }
+            }
+
+             Console.WriteLine("Goodbye and thanks from Wordle!");
+
+        }
+    }
+
+    class Wordle {
+        public Wordle(){
+            Console.WriteLine("Hello from Wordle!");
+        }
+
+        public void playWordle(){
+            Console.WriteLine("Apologies, playing not yet implemented");
+        }
+
+
+    }
+}
