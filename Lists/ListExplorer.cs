cba1f0f7f4c7b5b062a5635001075c4025d469b2
djnaSoft <88729193+djnaSoft@users.noreply.github.com>
2023-06-15 14:18:10 +0100

    add find

--- Lists/ListExplorer.cs
+++ Lists/ListExplorer.cs
@@ -10,34 +10,42 @@ using System.Collections;
         {
             Console.WriteLine("Hello, Lists!");
             ArrayList theArrayList = new ArrayList();
-            testArrayPerformance(theArrayList, 100000);
+            testArrayPerformance(theArrayList, 10000);
 
             LinkedList<int> theLinkedList = new LinkedList<int>();
-            testListPerformance(theLinkedList, 100000);
+            testListPerformance(theLinkedList, 10000);
         }
 
         private static void testListPerformance(LinkedList<int> numbers, int howMany){
 
             int startTime = Environment.TickCount;
+            Random r = new Random();
+            
             for (int i = 0; i < howMany; i++) {
-                numbers.AddLast( i);
+                int rInt = r.Next(0, howMany/2);
+                numbers.AddLast( rInt );
+                numbers.Find(rInt);
             }
             int endTime = Environment.TickCount;
 
-            Console.WriteLine("Total execution time for {0} items is {1}ms: ",howMany , (endTime - startTime));
+            Console.WriteLine("List execution time for {0} items is {1}ms: ",howMany , (endTime - startTime));
 
         }
 
         
-        private static void testArrayPerformance(IList numbers, int howMany){
+        private static void testArrayPerformance(ArrayList numbers, int howMany){
 
             int startTime = Environment.TickCount;
+
+            Random r = new Random();
             for (int i = 0; i < howMany; i++) {
-                numbers.Add( i);
+                int rInt = r.Next(0, howMany/2);
+                numbers.Add( rInt);
+                numbers.Contains(rInt);
             }
             int endTime = Environment.TickCount;
 
-            Console.WriteLine("Total execution time for {0} items is {1}ms: ",howMany , (endTime - startTime));
+            Console.WriteLine("Array execution time for {0} items is {1}ms: ",howMany , (endTime - startTime));
 
         }
 
